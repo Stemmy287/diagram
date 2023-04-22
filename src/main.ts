2a312aded741486db0100cd93eb6601565d080b1
Stemmy287 <steem287@gmail.com>
2023-04-22 11:54:06 +0300

    feat: button start/stop

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ let data = []
 
 const diagram = document.querySelector('.diagram')
 const button = document.querySelector('.btn')
+const iconButton = document.querySelector('img')
 
 const getData = async () => {
 
@@ -14,18 +15,34 @@ const getData = async () => {
   data = text.split("\n").filter(el => el).map(el => {
     const newArr = el.split(' ')
     return {name: newArr[0], num: newArr[1]}
-  }).sort((a,b) => b.num - a.num)
+  }).sort((a, b) => b.num - a.num)
 
   diagram.innerHTML = data.map((el, i) => {
 
-    const width = Math.floor(el.num  * 10)
+      const width = Math.floor(el.num * 10)
 
       return `<div class="scheduleContainer"><div class="num">${Math.floor(el.num)}</div>
 <div id="schedule" class="schedule" style="height: ${width}px; background-color: ${colors[i]}"></div>
-<span class="name">${el.name}</span></div> `
+<span class="name">${el.name}</span></div>`
     }
   ).join('')
 
 }
 
 getData()
+
+let timer = setInterval(getData, 5000)
+
+button.addEventListener('click', () => {
+
+  if (timer) {
+    timer = clearInterval(timer) as number
+    iconButton.setAttribute('src', 'src/img/play-svgrepo-com.svg')
+  } else {
+    getData()
+    timer = setInterval(getData, 5000)
+    iconButton.setAttribute('src', 'src/img/pause-svgrepo-com.svg')
+  }
+
+})
+
