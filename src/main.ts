4dc0745f94b566c22269dc553a2b2c21234e82b2
Stemmy287 <steem287@gmail.com>
2023-04-22 17:46:01 +0300

    feat: final refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,78 @@
 import './style.css'
 
-let data = []
+let data: Array<DataType> = []
 
 const button = document.querySelector('.btn')
 const iconButton = document.querySelector('img')
-const numScheduleContainer = document.querySelector('.schedule__num-column')
-const nameContainer = document.querySelector('.schedule__name')
+const numsAndColumnsContainer = document.querySelector('.schedule__nums-columns')
+const namesContainer = document.querySelector('.schedule__names')
 
-const getData = async () => {
-
-  const colors = ['#7858d7', '#ea643f', '#5baa73', '#f4a965']
+const fetchData = async () => {
 
   const url = import.meta.env.VITE_DATA_URL
 
   const res = await fetch(url)
   const text = await res.text()
-  data = text.split("\n").filter(el => el).map(el => {
-    const newArr = el.split(' ')
-    return {name: newArr[0], num: newArr[1]}
-  }).sort((a, b) => b.num - a.num)
 
-  numScheduleContainer.innerHTML = data.map((el, i) => {
+  data = text
+    .split("\n")
+    .filter(el => el)
+    .map(el => {
 
-      const width = Math.floor(el.num * 10)
+      const newArr = el.split(' ')
 
-      return `<div><div class="num">${Math.floor(el.num)}</div>
-              <div class="column" style="height: ${width}px; background-color: ${colors[i]}"></div></div>`
-    }
-  ).join('')
+      return {name: newArr[0], num: newArr[1]}
 
-  nameContainer.innerHTML = data.map((el) => {
-      return `<span class="name">${el.name}</span>`
-    }
-  ).join('')
+    }).sort((a, b) => b.num - a.num)
 
 }
 
-getData()
+const outputData = async () => {
 
-let timer = setInterval(getData, 5000)
+  const colors = ['#7858d7', '#ea643f', '#5baa73', '#f4a965']
+
+  try {
+
+    await fetchData()
+
+    numsAndColumnsContainer.innerHTML = data.map((el, i) => {
+
+        const width = Math.floor(+el.num) * 10
+
+        return `<div><div class="num">${Math.floor(+el.num)}</div>
+              <div class="column" style="height: ${!el.num ? el.num : width}px; background-color: ${colors[i]}"></div></div>`
+      }
+    ).join('')
+
+    namesContainer.innerHTML = data.map((el) => `<span class="name">${el.name}</span>`).join('')
+
+  } catch (e) {
+    console.log(e)
+  }
+
+}
+
+outputData()
+
+let timer = setInterval(outputData, 5000)
 
 button.addEventListener('click', () => {
 
   if (timer) {
-    timer = clearInterval(timer) as number
+    clearInterval(timer)
+    timer = 0
     iconButton.setAttribute('src', 'src/img/play-svgrepo-com.svg')
   } else {
-    getData()
-    timer = setInterval(getData, 5000)
+    outputData()
+    timer = setInterval(outputData, 5000)
     iconButton.setAttribute('src', 'src/img/pause-svgrepo-com.svg')
   }
 
 })
+
+//types
+type DataType = {
+  name: string
+  num: string
+}
 
