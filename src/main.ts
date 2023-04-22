beea2fdb166c4d5074020dbfc5f336672fbf0563
Stemmy287 <steem287@gmail.com>
2023-04-22 11:15:35 +0300

    feat: colors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import './style.css'
 let data = []
 
 const diagram = document.querySelector('.diagram')
+const button = document.querySelector('.btn')
 
 const getData = async () => {
+
+  const colors = ['#7858d7', '#ea643f', '#5baa73', '#f4a965']
+
   const res = await fetch('http://tt.centr-to.ru/frontend-2023.txt')
   const text = await res.text()
   data = text.split("\n").filter(el => el).map(el => {
@@ -12,12 +16,12 @@ const getData = async () => {
     return {name: newArr[0], num: newArr[1]}
   }).sort((a,b) => b.num - a.num)
 
-  diagram.innerHTML = data.map(el => {
+  diagram.innerHTML = data.map((el, i) => {
 
     const width = Math.floor(el.num  * 10)
 
       return `<div class="scheduleContainer"><div class="num">${Math.floor(el.num)}</div>
-<div id="schedule" class="schedule" style="height: ${width}px"></div>
+<div id="schedule" class="schedule" style="height: ${width}px; background-color: ${colors[i]}"></div>
 <span class="name">${el.name}</span></div> `
     }
   ).join('')
@@ -25,6 +29,3 @@ const getData = async () => {
 }
 
 getData()
-
-setInterval(getData, 5000)
-
